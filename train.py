740e4a47fa9011806feb9dd76f27a967a244b717
gabriel <gmaher2@hotmail.com>
2016-10-20 00:22:47 -0700

    need to debug loss funtion

--- train.py
+++ train.py
@@ -70,6 +70,38 @@ def normalize_bbox(bbox,W,H,S):
     h = bbox[3]*scaley
     return np.asarray([x,y,w,h])
 
+def getBboxLabels(yhat,y,W,H,S):
+    '''
+    computes which predicted boxes in yhat match those in  the true labels y
+
+    args:
+        @a yhat: predicted bounding boxes, shape = (Nbatch,S,S,4)
+        @a y: true label bounding boxes shape = (Nlabel,4)
+        @a W,H,S: image width, height and output dims
+    '''
+    inds = []
+    boxes = []
+    for bbox in y:
+        best_iou = 0.0
+        best_box = 0.0
+        best_box_pred = 0.0
+        for i in range(0,S):
+            for j in range(0,S):
+                bbox_pred_denorm = denormalize_bbox(yhat[0,i,j,:],W,H,S,i,j)
+                iou = calc_iou(bbox_pred_denorm, bbox)
+                if iou >= best_iou:
+                    best_iou = iou
+                    best_index = (i,j)
+                    best_box = bbox
+                    best_box_pred = bbox_pred_denorm
+        inds.append(best_index)
+        boxes.append(best_box)
+
+        print 'iou={}'.format(best_iou)
+        print 'boxpred={}'.format(best_box_pred)
+        print 'box={}'.format(best_box)
+    return boxes,inds
+
 X = np.random.randn(1,400,400,3)
 y = np.asarray([
     [100.5,50.6,50,70],
@@ -79,9 +111,9 @@ y = np.asarray([
 Nbatch,H,W,C = X.shape
 Nfilter = 16
 Wfilter = 3
-lr = 1e-3
+lr = 1e-4
+Niter = 1000
 
-#img = Input(shape=(H,W,C))
 sess = tf.Session()
 K.set_session(sess)
 
@@ -101,20 +133,37 @@ out_box = Convolution2D(4, Wfilter, Wfilter,
 out_conf = Convolution2D(1, Wfilter, Wfilter,
     activation='sigmoid', border_mode='same')(out)
 
+loss = tf.reduce_sum(tf.mul(true_conf,
+        tf.reduce_sum(tf.square(true_box-out_box),keep_dims=True)
+        )
+    ) + tf.reduce_sum(tf.square(true_conf-out_conf))
+
+opt = tf.train.AdamOptimizer(lr)
+train = opt.minimize(loss)
+
 yhat = sess.run(out_box,feed_dict={img:X})
 S = yhat.shape[1]
 
 P = np.zeros((1,S,S,1))
 Y = np.zeros(yhat.shape)
 
-for bbox in y:
-    best_iou = 0.0
-    for i in range(0,S):
-        for j in range(0,S):
-            bbox_pred_denorm = denormalize_bbox(yhat[0,i,j,:],W,H,S,i,j)
-            iou = calc_iou(bbox_pred_denorm, bbox)
-            if iou >= best_iou:
-                best_iou = iou
-                best_index = (i,j)
-    P[0,best_index[0],best_index[1],0] = 1.0
-    Y[0,best_index[0],best_index[1],:] = normalize_bbox(bbox,W,H,S)
+###########
+# Training
+###########
+init = tf.initialize_all_variables()
+sess.run(init)
+
+for step in xrange(Niter):
+    P[:]=0.0
+    Y[:]=0.0
+    yhat = sess.run(out_box,feed_dict={img:X})
+    boxes, inds = getBboxLabels(yhat,y,W,H,S)
+    for bbox,best_index in zip(boxes,inds):
+        P[0,best_index[0],best_index[1],0] = 1.0
+        Y[0,best_index[0],best_index[1],:] = normalize_bbox(bbox,W,H,S)
+        print bbox, Y[0,best_index[0],best_index[1],:],best_index
+    sess.run(train, feed_dict={img:X, true_box:Y, true_conf:P})
+
+    if step%20 == 0:
+        l = sess.run(loss, feed_dict={img:X, true_box:Y, true_conf:P})
+        print "loss={}".format(l)
